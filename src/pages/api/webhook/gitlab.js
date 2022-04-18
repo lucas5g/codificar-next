3d42dd61fa15b89aed123553408b81a3635dccef
lucas.sousa <lucas.sousa@codificar.com.br>
2022-04-18 20:04:06 -0300

    test webhook

--- src/pages/api/webhook/gitlab.js
+++ src/pages/api/webhook/gitlab.js
@@ -1,8 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { sendMessageRocket } from "../../../helpers"
+
 export default function gitlab(req, res) {
 
-    console.log(req.body)
+    // console.log(req.body)
+
+    const user = req.body.user || 'lucas.sousa'
+    const pipelineId = req.body.object_attributes ? req.body.object_attributes.id : '5399'
+    const commit = req.body.commit ? req.body.commit.url : "https://git.codificar.com.br/marketplace/web/-/commit/2564a5d48ab82d1772ba10441cd20f5ec89913b0"
+
+    const text = `
+        Erro no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}
+        @${user} verificar seu commit - ${commit}.
+        @lucas.sousa verifique os testes.
+    `
+
+    sendMessageRocket('@lucas.sousa', text)
+
 
     res.status(200).json({
         api: 'gitlab webhook',
