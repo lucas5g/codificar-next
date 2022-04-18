1e814586b4abdce65bdd93670f2acb44519e5656
lucas.sousa <lucas.sousa@codificar.com.br>
2022-04-18 20:25:49 -0300

    refac gitlab

--- src/pages/api/webhook/gitlab.js
+++ src/pages/api/webhook/gitlab.js
@@ -9,12 +9,20 @@ export default function gitlab(req, res) {
     const user = req.body.username || 'lucas.sousa'
     const pipelineId = req.body.object_attributes ? req.body.object_attributes.id : '5399'
     const commit = req.body.commit ? req.body.commit.url : "https://git.codificar.com.br/marketplace/web/-/commit/2564a5d48ab82d1772ba10441cd20f5ec89913b0"
+    const status = req.body.object_attributes ? req.body.object_attributes.status : 'false'
 
-    const text = `
-        Erro no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}
+    let text = ''
+    if (status === 'success') {
+        text = `Sucesso no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}
+        @${user} seu commit está ok - ${commit}.
+        `
+    } else {
+        text = `Erro no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}'
         @${user} verificar seu commit - ${commit}.
         @lucas.sousa verifique os testes.
-    `
+        `
+    }
+
 
     sendMessageRocket('@lucas.sousa', text)
 
