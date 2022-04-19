dfdf036cfad94c933012862c7d9676357a033628
lucas.sousa <lucas.sousa@codificar.com.br>
2022-04-18 21:09:10 -0300

    ponto no push

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git add .
 git commit -m "$1"
 git push
 ssh -t versionaplicativoderestaurante "cd codificar/codificar-next/ && git pull && pm2 restart codificar"
-echo "http://versionaplicativoderestaurante.com.br:3000"
+echo "http://version.aplicativoderestaurante.com.br:3000"
 # echo "test $1"
