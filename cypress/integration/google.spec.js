c0a64555163d3f84c2cdb90b7ba8061d655f49a9
lucas.sousa <lucas.sousa@codificar.com.br>
2022-04-20 12:48:12 -0300

    login no google nao é simples

--- cypress/integration/google.spec.js
+++ cypress/integration/google.spec.js
@@ -7,13 +7,20 @@ describe('login google', () => {
         // cy.visit('https://play.google.com/intl/pt-BR/console/about')
         // cy.get('.navbar-cta > .btn-primary').click()
 
-        cy.visit('https://accounts.google.com/signin/v2/identifier?service=androiddeveloper&passive=true&continue=https%3A%2F%2Fplay.google.com%2Fconsole%2Fdeveloper%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+        cy.visit('https://play.google.com/console/signup')
             // cy.visit('https://accounts.google.com/signin/v2/identifier?service=androiddeveloper&passive=true&continue=https%3A%2F%2Fplay.google.com%2Fconsole%2Fdeveloper%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
 
         // cy.log(Cypress.env('email'))
-        cy.get('#identifierId').type(Cypress.env('email'))
-        cy.contains('Próxima').click()
+        // cy.get('#identifierId').type(Cypress.env('email'))
+        cy.get('#Email').type(Cypress.env('email'))
+        cy.get('#next').click()
+            // cy.contains('Próxima').click()
 
-        cy.get('#password').type('qweqwe')
+        cy.get('#password').type(Cypress.env('password'))
+        cy.get('#submit').click()
+            // cy.contains('Próxima').click()
+
+        cy.wait(2000)
+            // cy.visit('https://play.google.com/console/u/0/developers/5887817223763081215/app-list?onboardingflow=signup')
     })
 })
