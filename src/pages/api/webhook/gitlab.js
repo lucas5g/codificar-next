e8196fec20d76c2fe802c97337310c7e0e7a441a
lucas.sousa <lucas.sousa@codificar.com.br>
2022-04-28 09:51:11 -0300

    send mensagem para o dev

--- src/pages/api/webhook/gitlab.js
+++ src/pages/api/webhook/gitlab.js
@@ -6,25 +6,28 @@ export default function gitlab(req, res) {
 
     // console.log(req.body)
 
-    const author = req.body.commit ? req.body.commit.author.email.replace('@codificar.com.br', '') : 'lucas.sousa'
+    // const author = req.body.commit ? req.body.commit.author.email.replace('@codificar.com.br', '') : 'lucas.sousa'
+    const username = req.body.user ? req.body.user.username : 'lucas.sousa'
     const pipelineId = req.body.object_attributes ? req.body.object_attributes.id : '5399'
     const commit = req.body.commit ? req.body.commit.url : "https://git.codificar.com.br/marketplace/web/-/commit/2564a5d48ab82d1772ba10441cd20f5ec89913b0"
     const status = req.body.object_attributes ? req.body.object_attributes.status : 'false'
 
     let text = ''
     if (status === 'success') {
-        text = `Sucesso no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}
-        @${author} seu commit está ok - ${commit}.
+        text = `Sucesso no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}\n
+        @${username} o último commit está ok :)
+        ${commit}.
         `
-        sendMessageRocket('@lucas.sousa', text)
+        sendMessageRocket(`@${username}`, text)
+
 
     } else if (status === 'failed') {
-        text = `Erro no pipeline - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId}
-        @${author} verificar seu commit - ${commit}.
+        text = `\n
+        *Erro no pipeline* - https://git.codificar.com.br/marketplace/web/-/pipelines/${pipelineId} :(\n
+        @${username} verificar seu commit - ${commit}.
         @lucas.sousa verifique os testes.
         `
-        sendMessageRocket('@lucas.sousa', text)
-
+        sendMessageRocket(`@${username}`, text)
     }
 
 
