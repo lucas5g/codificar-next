7775d708b8601f1a45bf8092e9acdfe4f2925cff
lucas5g <lucasdesousa19@hotmail.com>
2022-05-19 10:48:40 -0300

    report reabertas

---
+++ src/pages/api/issues/reabertas.js
@@ -0,0 +1,22 @@
+import { devUserRocket, sendMessageRocket } from "../../../helpers"
+import { apiRedmine } from "../../../services/api"
+
+export default async function handler(req, res) {
+
+    const { data } = await apiRedmine.get('/issues.json?status_id=8')
+    console.log(data)
+
+
+    data.issues.map(issue => {
+        const text = `*REABERTA* - https://redmine.codificar.com.br/issues/${issue.id} - ${issue.subject}`
+        console.log(text)
+        console.log(devUserRocket(issue))
+        sendMessageRocket('@lucas.sousa', text)
+        sendMessageRocket(devUserRocket(issue))
+    })
+
+    res.json({
+        msg: 'reabert',
+        issues: data.issues
+    })
+}
