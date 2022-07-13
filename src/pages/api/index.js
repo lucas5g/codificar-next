a888232f2a4d43387ad6870e80cdf22ea2151c50
lucas5g <lucasdesousa19@hotmail.com>
2022-07-13 01:16:43 -0300

    fix list projects

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,10 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { prisma } from "../../../prisma/index.mjs"
-
 export default async function handler(req, res) {
 
-    const projects = await prisma.project.findMany()
     res.status(200).json({ api: 'cods' })
 
 
