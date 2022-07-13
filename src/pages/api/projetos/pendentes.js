8da3736b552f1c7004fad27ce2dd9c16341422f2
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2022-07-13 17:47:55 +0000

    api route projectos pendentes

---
+++ src/pages/api/projetos/pendentes.js
@@ -0,0 +1,62 @@
+import { prisma } from "../../../../prisma/index.js"
+import { getLastTagReact, getLastTagWeb } from "../../../utils/fetch"
+
+export default async function handler(req, res) {
+
+
+    // return res.json({msg: 'teste api'})
+
+    const lastTagWeb = await getLastTagWeb()
+    const lastTagReact = await getLastTagReact()
+
+    const projectsWeb = await prisma.project.findMany({
+        where: {
+            status: true,
+            NOT: {
+                versionWeb: lastTagWeb
+            }
+
+        }
+    })
+
+    const web = {
+        lastTag: lastTagWeb,
+        projects: projectsWeb
+    }
+
+    const projectsAndroid = await prisma.project.findMany({
+        where: {
+            status: true,
+            NOT: {
+                versionAndroid: lastTagReact
+            }
+
+        },
+        select: {
+            name: true,
+            versionAndroid: true,
+            urlUploadAndroid: true,
+            extensionAndroid: true,
+        }
+    })
+
+    projectsAndroid.map(project => project.name = project.name
+        .replace('molde restaurante', 'molde_restaurante')
+        .replace('medicol', 'medicolappmarketplace')
+        .replace(' ', '')
+        .replace('á', 'a')
+        .replace('ã', 'a')
+        .replace('ç', 'c')
+    )
+
+    const android = {
+        lastTag: lastTagReact,
+        projects: projectsAndroid
+    }
+
+    // console.log({ projectsName })
+    res.json({web, android})
+    return
+}
+
+
