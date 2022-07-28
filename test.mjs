427d7de6dbbdfb65d1c1907b6a1f37e07c1d4aab
lucas5g <lucasdesousa19@hotmail.com>
2022-07-28 13:14:32 -0300

    closed all orders

--- test.mjs
+++ test.mjs
@@ -1,33 +1,69 @@
 import cron from 'node-cron'
 import axios from 'axios';
 import 'dotenv/config'
+import moment from 'moment';
 
 
 (async() => {
 
-    const { data } = await  axios.get('https://redmine.codificar.com.br/issues.json', {
+    const { data } = await axios.get('https://redmine.codificar.com.br/issues.json', {
         params: {
             project_id: 'uber-servicos',
             key: process.env.REDMINE_KEY,
-            limit: 100
+            limit: 100,
+            // status_id: 'closed'
 
         }
     })
 
     const { total_count } = data
 
-    const issues = data.issues.map( issue => {
+    const issues = data.issues.map(issue => {
         return {
             id: issue.id,
-            subject: issue.subject
+            subject: issue.subject,
+            client: issue.project.name,
+            status: issue.status,
+            assigned_to: issue.assigned_to,
+            creationDays: moment().diff(issue.created_on, 'days'),
+            // ...issue
         }
-        
-    }) 
 
-    console.log({
-        total_count,
-        issues
     })
+
+    const closeTask = issues.filter(issue => issue.client !== 'MoviCare' && issue.creationDays > 90)
+
+    // closeTask.forEach((issue, index) => (
+    //     console.log(`${index + 1} - https://redmine.codificar.com.br/issues/${issue.id} - ${issue.client} - ${issue.subject} - ${issue.creationDays}`)
+    // ))
+
+
+
+    closeTask.map((issue, index) => {
+        setTimeout(() => {
+            const url = `https://redmine.codificar.com.br/issues/${issue.id}.json?key=${process.env.REDMINE_KEY}`
+            console.log(url)
+            axios.put(url, {
+                "issue": {
+                    "assigned_to_id": 338,
+                    "status_id": 6,
+                    "start_date": "2022-07-28",
+                    "due_date": "2022-07-28",
+                    "estimated_hours": 1
+                }
+            })
+        }, index * 677)
+    })
+
+    // {
+    //     assigne
+    // }
+    // console.log({
+    //     issues,
+    //     total_count,
+    //     closeTaskLenght: closeTask.length
+    //         // issues
+    // })
 
 })()
 
